212fb5b6ac91e87f0db03415b33a48d1b8dbcb34
Alexandre PACCOU <a.paccou@coteo.com>
2015-04-27 13:57:37 +0200

    Ajout du controller et de l'icone de package

---
+++ controller.php
@@ -0,0 +1,32 @@
+<?php
+namespace Concrete\Package\CoteoBoilerplate;
+
+use \Concrete\Core\Package\Package;
+use \Concrete\Core\Page\Theme\Theme;
+
+defined('C5_EXECUTE') or die('Access Denied.');
+
+class Controller extends Package
+{
+  protected $pkgHandle = 'coteo_boilerplate_package';
+  protected $appVersionRequired = '5.7.3';
+  protected $pkgVersion = '0.0.1';
+
+  public function getPackageName()
+  {
+    return t("Concrete 5.7 Boilerplate Theme Package");
+  }
+
+  public function getPackageDescription()
+  {
+    return t("A package that installs a boilerplate theme for 5.7.3.");
+  }
+
+  public function install()
+  {
+    $pkg = parent::install();
+
+    //Install the theme
+    $theme = PageTheme::add('theme_vitrine_uikit', $pkg);
+  }
+}
