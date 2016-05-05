e4aca4aa5e2771ceccc07484a822dd2bd13b4a6d
Alexandre PACCOU <a.paccou@coteo.com>
2016-05-05 23:34:53 +0200

    Modification de l'ordre d'appel dans le header pour raison de sécurité

--- themes/theme_vitrine_uikit/elements/header.php
+++ themes/theme_vitrine_uikit/elements/header.php
@@ -1,15 +1,13 @@
 <?php defined('C5_EXECUTE') or die("Access Denied."); ?>
 <!DOCTYPE html>
 <html lang="<?php echo Localization::activeLanguage(); ?>">
-<head>    
-    <?php echo $html->css($view->getStyleSheet('application.less')); ?>
+<head>
     <?php Loader::element('header_required', array('pageTitle' => isset($pageTitle) ? $pageTitle : '', 'pageDescription' => isset($pageDescription) ? $pageDescription : ''));?>
+    <?php echo $html->css($view->getStyleSheet('application.less')); ?>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     <link rel="canonical" href="<?php echo $url = $c->getCollectionLink(); ?>" />
-
-
 
 </head>
 <body>
