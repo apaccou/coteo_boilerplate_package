a5161638a5234d6c03a8d6ca7ea5e7563d54b95d
Alexandre PACCOU <a.paccou@coteo.com>
2016-05-08 16:50:41 +0200

    Ajout des crédits

--- themes/theme_vitrine_uikit/elements/footer.php
+++ themes/theme_vitrine_uikit/elements/footer.php
@@ -4,10 +4,10 @@
   <div class="footer-main">
   </div>
   <div class="footer-bottom">
+    <p>Réalisation <a href="http://wwww.coteo.com/">Agence Web Coteo</a> &copy; 2016<?php if(date('Y') != 2016) {echo ' - ' . date('Y');}?></p>
+    <p><a href="#" class="top" title="Jump to the top of the page">&uarr; Back to top</a></p>
   </div>
 </footer>
-
-<p><a href="#" class="top" title="Jump to the top of the page">&uarr; Back to top</a></p>
 
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
