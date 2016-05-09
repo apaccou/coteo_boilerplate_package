331173f82bf8f6767e9101c47810247e05a4e464
Alexandre PACCOU <a.paccou@coteo.com>
2016-05-09 23:09:57 +0200

    Ajout balise semantique et retour haut de page à choisir

--- themes/theme_vitrine_uikit/elements/footer.php
+++ themes/theme_vitrine_uikit/elements/footer.php
@@ -4,8 +4,9 @@
   <div class="footer-main">
   </div>
   <div class="footer-bottom">
-    <p>Réalisation <a href="http://wwww.coteo.com/">Agence Web Coteo</a> &copy; 2016<?php if(date('Y') != 2016) {echo ' - ' . date('Y');}?></p>
+    <p>Réalisation <a rel="license" href="http://wwww.coteo.com/">Agence Web Coteo</a> &copy; 2016<?php if(date('Y') != 2016) {echo ' - ' . date('Y');}?></p>
     <p><a href="#" class="top" title="Jump to the top of the page">&uarr; Back to top</a></p>
+    <p><a id="back-to-top" href="#top" data-uk-smooth-scroll><i class="uk-icon-chevron-circle-up"></i></a></p>
   </div>
 </footer>
 
